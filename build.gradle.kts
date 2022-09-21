73e7ff8a3eb525450f14ef883e00777d4ed7db75
Durgan <durganmcbroom@gmail.com>
2022-09-21 07:44:16 -0700

    Added github actions, first test with that.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     kotlin("jvm") version "1.7.10"
     id("org.javamodularity.moduleplugin") version "1.8.12"
+
+    id("signing")
+    id("maven-publish")
+    id("org.jetbrains.dokka") version "1.6.0"
 }
 
 group = "net.yakclient"
@@ -8,15 +12,15 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    mavenLocal()
-//    maven {
-//        name = "Durgan McBroom GitHub Packages"
-//        url = uri("https://maven.pkg.github.com/durganmcbroom/artifact-resolver")
-//        credentials {
-//            username = project.findProperty("gpr.user") as? String ?: throw IllegalArgumentException("Need a Github package registry username!")
-//            password = project.findProperty("gpr.key") as? String ?: throw IllegalArgumentException("Need a Github package registry key!")
-//        }
-//    }
+//    mavenLocal()
+    maven {
+        name = "Durgan McBroom GitHub Packages"
+        url = uri("https://maven.pkg.github.com/durganmcbroom/artifact-resolver")
+        credentials {
+            username = project.findProperty("gpr.user") as? String ?: throw IllegalArgumentException("Need a Github package registry username!")
+            password = project.findProperty("gpr.key") as? String ?: throw IllegalArgumentException("Need a Github package registry key!")
+        }
+    }
     maven {
         isAllowInsecureProtocol = true
         url = uri("http://repo.yakclient.net/snapshots")
@@ -50,41 +54,152 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+task<Jar>("sourcesJar") {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+task<Jar>("javadocJar") {
+    archiveClassifier.set("javadoc")
+    from(tasks.dokkaJavadoc)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("boot-maven") {
+            from(components["java"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+
+            artifactId = "boot"
+
+            pom {
+                name.set("Boot")
+                description.set("YakClient's Boot module")
+                url.set("https://github.com/yakclient/boot")
+
+                packaging = "jar"
+
+                developers {
+                    developer {
+                        id.set("Chestly")
+                        name.set("Durgan McBroom")
+                    }
+                }
+
+                licenses {
+                    license {
+                        name.set("GNU General Public License")
+                        url.set("https://opensource.org/licenses/gpl-license")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/yakclient/boot")
+                    developerConnection.set("scm:git:ssh://github.com:yakclient/boot.git")
+                    url.set("https://github.com/yakclient/boot")
+                }
+            }
+        }
     }
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
+allprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "maven-publish")
 
-tasks.compileJava {
-    targetCompatibility = "17"
-    sourceCompatibility = "17"
-}
+    group = "net.yakclient"
+    version = "1.0-SNAPSHOT"
 
-tasks.compileJava {
-    sourceCompatibility = "17"
-    targetCompatibility = "17"
-}
+    repositories {
+        mavenCentral()
+        maven {
+            isAllowInsecureProtocol = true
+            url = uri("http://repo.yakclient.net/snapshots")
+        }
+    }
 
-tasks.compileKotlin {
-    destinationDirectory.set(tasks.compileJava.get().destinationDirectory.asFile.get())
-    kotlinOptions.jvmTarget = "17"
-}
+    publishing {
+        repositories {
+            if (!project.hasProperty("maven-user") || !project.hasProperty("maven-pass")) return@repositories
 
-tasks.compileTestKotlin {
-    kotlinOptions.jvmTarget = "17"
-}
+            maven {
+                val repo = if (project.findProperty("isSnapshot") == "true") "snapshots" else "releases"
 
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+                isAllowInsecureProtocol = true
+
+                url = uri("http://repo.yakclient.net/$repo")
+
+                credentials {
+                    username = project.findProperty("maven-user") as String
+                    password = project.findProperty("maven-pass") as String
+                }
+                authentication {
+                    create<BasicAuthentication>("basic")
+                }
+            }
+        }
+    }
+
+    kotlin {
+        explicitApi()
+    }
+
+    dependencies {
+        implementation(kotlin("stdlib"))
+        implementation(kotlin("reflect"))
+        testImplementation(kotlin("test"))
+    }
+
+    tasks.compileKotlin {
+        destinationDirectory.set(tasks.compileJava.get().destinationDirectory.asFile.get())
+
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
+
+    tasks.compileTestKotlin {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
+
+    tasks.test {
+        useJUnitPlatform()
+    }
+
+    tasks.compileJava {
+        targetCompatibility = "17"
+        sourceCompatibility = "17"
+    }
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(17))
+        }
     }
 }
-
-kotlin {
-    explicitApi()
-}
+//
+//
+//tasks.test {
+//    useJUnitPlatform()
+//}
+//
+//tasks.compileJava {
+//    targetCompatibility = "17"
+//    sourceCompatibility = "17"
+//}
+//
+//tasks.compileJava {
+//    sourceCompatibility = "17"
+//    targetCompatibility = "17"
+//}
+//
+//tasks.compileKotlin {
+//    destinationDirectory.set(tasks.compileJava.get().destinationDirectory.asFile.get())
+//    kotlinOptions.jvmTarget = "17"
+//}
+//
+//tasks.compileTestKotlin {
+//    kotlinOptions.jvmTarget = "17"
+//}
