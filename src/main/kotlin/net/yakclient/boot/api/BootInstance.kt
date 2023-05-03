eda6b48cd2ae5278433c62e861bda850b23333c6
Durgan <durganmcbroom@gmail.com>
2023-05-02 20:01:17 -0700

    Took out module system references. Commented out some deprecated security policy stuff.

---
+++ src/main/kotlin/net/yakclient/boot/api/BootInstance.kt
@@ -0,0 +1,11 @@
+package net.yakclient.boot.api
+
+import net.yakclient.boot.BootContext
+import net.yakclient.boot.dependency.DependencyProviders
+
+public class BootInstance internal constructor(
+    public val context: BootContext,
+
+) {
+
+}
